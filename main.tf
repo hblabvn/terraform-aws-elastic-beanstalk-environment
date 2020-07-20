06fc88f45f1396b1b18a213c2122dced20e93a1f
cuongvv <cuongvv@hblab.vn>
2020-07-20 10:03:56 +0700

    Modify LB setting

--- main.tf
+++ main.tf
@@ -192,30 +192,6 @@ resource "aws_elastic_beanstalk_environment" "default" {
   }
 
   setting {
-    namespace = "aws:ec2:instances"
-    name      = "EnableSpot"
-    value     = var.enable_spot_instances ? "true" : "false"
-  }
-
-  setting {
-    namespace = "aws:ec2:instances"
-    name      = "SpotFleetOnDemandBase"
-    value     = var.spot_fleet_on_demand_base
-  }
-
-  setting {
-    namespace = "aws:ec2:instances"
-    name      = "SpotFleetOnDemandAboveBasePercentage"
-    value     = var.spot_fleet_on_demand_above_base_percentage == -1 ? (var.environment_type == "LoadBalanced" ? 70 : 0) : var.spot_fleet_on_demand_above_base_percentage
-  }
-
-  setting {
-    namespace = "aws:ec2:instances"
-    name      = "SpotMaxPrice"
-    value     = var.spot_max_price == -1 ? "null" : var.spot_max_price
-  }
-
-  setting {
     namespace = "aws:autoscaling:launchconfiguration"
     name      = "EC2KeyName"
     value     = var.keypair
@@ -291,17 +267,17 @@ resource "aws_elastic_beanstalk_environment" "default" {
     value     = "${var.nodejs_version}"
   }
   
-  setting {
-    namespace = "aws:elasticbeanstalk:environment:process:default"
-    name      = "Port"
-    value     = var.application_port
-  }
+  # setting {
+  #   namespace = "aws:elasticbeanstalk:environment:process:default"
+  #   name      = "Port"
+  #   value     = var.application_port
+  # }
 
-  setting {
-    namespace = "aws:elasticbeanstalk:environment:process:default"
-    name      = "Protocol"
-    value     = "TCP"
-  }
+  # setting {
+  #   namespace = "aws:elasticbeanstalk:environment:process:default"
+  #   name      = "Protocol"
+  #   value     = "TCP"
+  # }
 
   setting {
     namespace = "aws:elasticbeanstalk:environment"
