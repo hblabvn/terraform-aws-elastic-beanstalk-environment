78e3d256c6af263fa3f93905e31bc9e41ec10ae4
cuongvv <cuongvv@hblab.vn>
2020-07-20 11:04:38 +0700

    Modify EB Security group

--- main.tf
+++ main.tf
@@ -81,6 +81,12 @@ resource "aws_elastic_beanstalk_environment" "default" {
 
   setting {
     namespace = "aws:autoscaling:launchconfiguration"
+    name      = "SSHSourceRestriction"
+    value     = "tcp,22,22,${var.ssh_source_restriction}"
+  }
+
+  setting {
+    namespace = "aws:autoscaling:launchconfiguration"
     name      = "SecurityGroups"
     value     = join(",", compact(concat([aws_security_group.default.id], sort(var.additional_security_groups))))
     resource  = ""
